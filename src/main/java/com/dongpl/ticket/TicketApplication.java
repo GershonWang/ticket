a6584041b17ad6c4a312ef1d89b839f045883c1f
GershonWang <986432015@qq.com>
2023-07-12 16:08:31 +0800

    ceshi

--- src/main/java/com/dongpl/ticket/TicketApplication.java
+++ src/main/java/com/dongpl/ticket/TicketApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TicketApplication extends AbstractJavaFxApplicationSupport {
 
     public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/selenium/chromedriver/linux64/114.0.5735.90/chromedriver");
 //        launch(TicketApplication.class, LoginView.class, new CustomLoadingView(), args);
         launch(TicketApplication.class, LoginView.class, args);
     }
--- src/main/java/com/dongpl/ticket/service/impl/SeleniumServiceImpl.java
+++ src/main/java/com/dongpl/ticket/service/impl/SeleniumServiceImpl.java
@@ -2,6 +2,10 @@ package com.dongpl.ticket.service.impl;
 
 import com.dongpl.ticket.service.SeleniumService;
 import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.stereotype.Service;
@@ -12,12 +16,26 @@ public class SeleniumServiceImpl implements SeleniumService {
     @Override
     public void start() {
         Platform.runLater(() -> {
-            System.setProperty("webdriver.chrome.driver", "src/main/resources/selenium/chromedriver/linux64/114.0.5735.90/chromedriver");
             WebDriver driver = new ChromeDriver();
-            driver.get("https://detail.damai.cn/item.htm?spm=a2oeg.home.card_0.ditem_0.591b23e1kV4fgA&id=602770049661");
-            String title = driver.getTitle();
-            System.out.println(title);
-//        driver.quit();
+            driver.get("https://www.damai.cn/");
+            Stage primaryStage = new Stage();
+            // 创建 JavaFX WebView
+            WebView webView = new WebView();
+            // 设置 WebView 的宽度和高度
+            webView.setPrefSize(600, 800);
+            // 将 Selenium 控制的网页加载到 WebView 中
+            webView.getEngine().load(driver.getCurrentUrl());
+            // 创建 JavaFX 布局
+            VBox root = new VBox(webView);
+            // 创建场景并显示主舞台
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Selenium WebView Example");
+            primaryStage.show();
+            // 关闭浏览器驱动程序时停止 Selenium 进程
+            primaryStage.setOnCloseRequest(event -> {
+                driver.quit();
+            });
         });
     }
 
